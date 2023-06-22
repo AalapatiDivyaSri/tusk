d66ee2a1b8a1fa8fbaaca9fde9c37a9a68088656
AalapatiDivyaSri <divya601@sasi.ac.in>
2023-06-22 16:30:58 +0530

    first commit

---
+++ src/One.java
@@ -0,0 +1,36 @@
+import java.lang.String;
+public class One {
+
+	/*public static void main(String[] args) {
+		/*int a=5;//reference type
+		a=15;
+		a=45;
+		System.out.println(a);
+	String s="hii";                          //literal string 
+	String s1="hello";                        
+	String s2=new String("hii");              //using new keyword
+	String s3=new String("hello");
+	String s4="hii";
+	String s5=new String("hii");
+	System.out.println(s);
+	}*/
+
+		/* public static void main(String args[]){  
+		   String s="Sachin";  
+		   s=s.concat(" Tendulkar");  
+		   System.out.println(s);  
+		 }*/
+	  
+	
+	
+		 public static void main(String args[]){  
+		   String s1="Sachin ";  
+		   String s2="Tendulkar";  
+		   String s3=s1.concat(s2);  
+		   System.out.println(s3);//Sachin Tendulkar  
+		  }  
+		}  
+		  
+	
+
+
---
+++ src/StringEx1.java
@@ -0,0 +1,22 @@
+
+public class StringEx1 {
+public static void main(String args[])
+{
+	String s="divya";
+	String s1="divya";
+  System.out.println(s.length());
+  System.out.println(s.toUpperCase());
+  System.out.println(s.toLowerCase());
+  System.out.println(s.compareTo(s1));
+  System.out.println(s.charAt(3));
+  System.out.println(s.indexOf("a"));
+  System.out.println(s.startsWith("a"));
+  System.out.println(s.endsWith("p"));
+  System.out.println(s.equals(s1));
+  System.out.println(s.trim());
+  System.out.println(s.split(s1));
+  System.out.println(s==s1);
+  
+	
+}
+}
